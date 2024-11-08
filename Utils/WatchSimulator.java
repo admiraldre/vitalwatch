b6ed98f1eb02ff83bd9b18ac174414b09e9e2396
Andrei Vivar <drevivar2001@gmail.com>
2024-11-07 17:21:46 -0800

    modified to update every 5 seconds

---
+++ Utils/WatchSimulator.java
@@ -0,0 +1,19 @@
+public class HealthMetricsSimulator {
+    private static final Random random = new Random();
+
+    public static int generateHeartRate() {
+        return 60 + random.nextInt(40); // Random heart rate between 60 and 100 bpm
+    }
+
+    public static void startSimulation(HeartRateModel model) {
+        while (true) {
+            int newHeartRate = generateHeartRate();
+            model.updateHeartRate(newHeartRate);
+            try {
+                Thread.sleep(5000); // Wait for 10 seconds before updating again
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
