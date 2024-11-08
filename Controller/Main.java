31ff7bddb32b123a68a2da01424bc0b7480608c5
Andrei Vivar <drevivar2001@gmail.com>
2024-11-07 17:18:04 -0800

    moved Main.java to parent

--- Controller/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        HeartRateModel model = new HeartRateModel();
-        HeartRateView view = new HeartRateView();
-        HeartRateController controller = new HeartRateController(model, view);
-
-        controller.start(); // Start the simulation and observer pattern
-    }
-}
