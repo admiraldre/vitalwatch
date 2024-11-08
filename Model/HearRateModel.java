e98a8c56aeb3df748bd7676cdcedf8090200d658
Andrei Vivar <drevivar2001@gmail.com>
2024-11-07 17:11:18 -0800

    initialized HeartRateModel that stores heart related data

---
+++ Model/HearRateModel.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class HeartRateModel extends Observable {
+    private int heartRate;
+
+    public void updateHeartRate(int heartRate) {
+        this.heartRate = heartRate;
+        setChanged();
+        notifyObservers(); // Notify observers when the heart rate updates
+    }
+
+    public int getHeartRate() {
+        return heartRate;
+    }
+}
