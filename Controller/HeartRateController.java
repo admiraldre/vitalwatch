3581ab3a43365f96be85dd353064b15a0654b5bb
Andrei Vivar <drevivar2001@gmail.com>
2024-11-07 17:14:18 -0800

    added Controller part of MVC

---
+++ Controller/HeartRateController.java
@@ -0,0 +1,14 @@
+public class HeartRateController {
+    private HeartRateModel model;
+    private HeartRateView view;
+
+    public HeartRateController(HeartRateModel model, HeartRateView view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void start() {
+        model.addObserver(view); // Add view as an observer of the model
+        HealthMetricsSimulator.startSimulation(model); // Start the simulation
+    }
+}
